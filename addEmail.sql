eb0a0ba5404ea16490fe1705021c2f150ba875ae
karina <karinadantasluiz@gmail.com>
2023-07-17 15:15:56 -0300

    Update addEmail.sql

--- addEmail.sql
+++ addEmail.sql
@@ -1,65 +1,60 @@
-USE adventureworks2019 
+USE adventureworks2019;
+GO
 
-GO 
-
-if exists (select 1 from sys.objects where name = 'addnewemail')
-begin
-
-drop procedure addnewemail
-end
-
-go
-
-declare
-@emailNew VARCHAR(50) = 'karinadantasluiz@gmail.com'
-
-
+IF EXISTS (SELECT 1 FROM sys.objects WHERE name = 'addnewemail')
 BEGIN
+    DROP PROCEDURE addnewemail;
+END
+GO
 
-   BEGIN TRAN addemail 
-      
+CREATE PROCEDURE addnewemail  
+    @emailNew VARCHAR(50) = 'email-generico@gmail.com'
+AS
+BEGIN
+    BEGIN TRAN addemail;
 
-	  SET IDENTITY_INSERT [Person].[EmailAddress] ON;
+    SET IDENTITY_INSERT [Person].[EmailAddress] ON;
 
-INSERT INTO
-   [Person].[emailaddress] 
-   (
-   [BusinessEntityID],
-	[EmailAddressID],
-	[EmailAddress],
-	[rowguid],
-	[ModifiedDate]
-   )
-VALUES
-   (
-    (SELECT MAX ([BusinessEntityID]) + 1 FROM [Person].[EmailAddress]), 
-	(SELECT MAX ([EmailAddressID]) + 1 FROM [Person].[EmailAddress]),
-	'karinadantasluiz@gmail.com', 
-	Newid(), 
-	Getdate() 
-   )
-	SET IDENTITY_INSERT [Person].[EmailAddress] OFF;
+    INSERT INTO [Person].[EmailAddress] 
+    (
+        [BusinessEntityID],
+        [EmailAddressID],
+        [EmailAddress],
+        [rowguid],
+        [ModifiedDate]
+    )
+    VALUES
+    (
+        (SELECT MAX([BusinessEntityID]) + 1 FROM [Person].[EmailAddress]), 
+        (SELECT MAX([EmailAddressID]) + 1 FROM [Person].[EmailAddress]),
+        'email-generico@gmail.com', 
+        NEWID(), 
+        GETDATE() 
+    );
 
-SELECT
-   @emailNew = Count(*) 
-FROM
-   [Person].[emailaddress] 
-WHERE
-   [emailaddress] = 'karinadantasluiz@gmail.com' 
-   
-   IF @emailNew > 1 
-   BEGIN
-      ROLLBACK TRAN addemail PRINT 'this e-mail already in use' 
-   END
-   ELSE
-      BEGIN
-         COMMIT TRAN addemail PRINT 'e-mail added succesfully' 
-      END
+    SET IDENTITY_INSERT [Person].[EmailAddress] OFF;
+
+    DECLARE @emailCount INT;
+
+    SELECT @emailCount = Count(*) 
+    FROM [Person].[EmailAddress] 
+    WHERE [EmailAddress] = 'email-generico@gmail.com';
+
+    IF @emailCount > 1 
+    BEGIN
+        ROLLBACK TRAN addemail;
+        PRINT 'This email is already in use.';
+    END
+    ELSE
+    BEGIN
+        COMMIT TRAN addemail;
+        PRINT 'Email added successfully.';
+    END;
 END;
+GO
 
+-- Test the stored procedure
+-- EXEC addnewemail 'email-generico@gmail.com';
 
---exec addnewemail 'karinadantasluiz@gmail.com'
- 
-
- --select * from [Person].[EmailAddress] 
- --where [EmailAddress] =  'karinadantasluiz@gmail.com'
+-- SELECT statement to verify the inserted email address
+-- SELECT * FROM [Person].[EmailAddress] WHERE [EmailAddress] = 'email-generico@gmail.com';
